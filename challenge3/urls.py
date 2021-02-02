e59d60c43a7b40899bc8916d537071340e04a6ce
juliobriones <julesc00@hotmail.com>
2021-02-01 20:19:15 -0600

    Some file formatting to PEP8.

--- challenge3/urls.py
+++ challenge3/urls.py
@@ -19,16 +19,21 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("accounts.urls")),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="password_reset"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_complete"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="password_reset"),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"),
+         name="password_reset_complete"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
